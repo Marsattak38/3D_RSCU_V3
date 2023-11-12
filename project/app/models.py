5c878d43955a5a8eb79fc789fb615722d2204998
Marsattak38 <110765175+Marsattak38@users.noreply.github.com>
2023-11-12 12:39:12 +0100

    12/11/2023
    Axel's work finish

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Plan(models.Model):
+    nom = models.CharField(max_length=255)
+    fichier = models.FileField(upload_to='project/media')
+
+class Print(models.Model):
+    nom = models.CharField(max_length=255)
+    fichier = models.FileField(upload_to='project/media')
--- project/app/views.py
+++ project/app/views.py
@@ -1,45 +1,25 @@
 from django.shortcuts import render
+from .models import Plan, Print
 
-def verificationPayement(eventId):
-    import stripe
-
-    # Configurez votre clé secrète Stripe
-    stripe.api_key = 'sk_test_51Ms8bXANSuV8oWuSRIXT85HvDkOQqDMSNcTJV3XUDzUpLxtwi6jdpPJGIoazMwPPOz3fIH5KZF9ASsqYO0hUcOQF00Ql3JmpWH'
-
-    # Remplacez 'event_id' par l'ID de l'événement de paiement Stripe que vous souhaitez récupérer
-    event_id = eventId
-
-    try:
-        # Récupérez l'événement de paiement à partir de l'ID
-        payment_event = stripe.Event.retrieve(event_id)
-
-        # Vérifiez si l'événement est un événement de paiement réussi
-        if payment_event['type'] == 'payment_intent.succeeded':
-            payment_intent = payment_event['data']['object']  # Détails du paiement réussi
-            print(f"Confirmation de paiement réussi : {payment_intent}")
-            return True
-
-        else:
-            print(f"L'événement n'est pas un événement de paiement réussi : {payment_event}")
-
-    except stripe.error.StripeError as e:
-        print(f"Erreur Stripe : {e}")
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
 
 def modelisation(request):
-    if request.method == 'POST':
-        uploaded_file = request.FILE['file']
-        if verificationPayement(evt_1Nyv6jANSuV8oWuSjJvHFExM) == True:
-            print("success")
+
 
         
 
     return render(request, 'app/modelisation.html')
 
 def printing(request):
+    if request.method == 'POST':
+        nom = request.POST['nom']
+        fichier = request.FILES['fichier']
+        nouveau_fichier = Print(nom=nom, fichier=fichier)
+        nouveau_fichier.save()
+        return render(request, 'app/success.html')
     return render(request, 'app/printing.html')
 
 
